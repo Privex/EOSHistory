52768b2479ad12e3e3d5af4916704d23e963ac98
Chris (Someguy123) <info@someguy123.com>
2020-01-20 02:45:37 +0000

    chunked block deletion

--- historyapp/management/commands/delete_blocks.py
+++ historyapp/management/commands/delete_blocks.py
@@ -1,9 +1,14 @@
 import sys
 
+from django import db
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from historyapp.models import EOSBlock
+
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -35,12 +40,12 @@ class Command(BaseCommand):
                 print(" !!! You can only specify --start / --end OR individual blocks.")
                 return sys.exit(1)
             
-            print(" >>> Deleting blocks:", options['blocks'])
+            log.info(" >>> Deleting blocks: %s", options['blocks'])
             with transaction.atomic():
                 blocks = EOSBlock.objects.filter(number__in=options['blocks'])
                 res = blocks.delete()
-            print(f" [+++] Finished deleting {res[0]} blocks within: {options['blocks']}")
-            print(" [+++] Objects deleted:", res[1])
+            log.info(f" [+++] Finished deleting {res[0]} blocks within: {options['blocks']}")
+            log.info(" [+++] Objects deleted: %s", res[1])
             return
         elif options['end'] is not None and options['start'] is not None:
             start_block, end_block = int(options['start']), int(options['end'])
@@ -50,12 +55,43 @@ class Command(BaseCommand):
                 print(" !!! The start block cannot be negative, and end block must be at least 1")
                 return sys.exit(1)
             blocks_deleting = (end_block - start_block) + 1
-            print(f" >>> Deleting UP TO {blocks_deleting} blocks between (and including) {start_block} and {end_block}")
-            with transaction.atomic():
-                blocks = EOSBlock.objects.filter(number__gte=start_block, number__lte=end_block)
-                res = blocks.delete()
-            print(f" [+++] Successfully deleted {res[0]} blocks between (and including) {start_block} and {end_block}")
-            print(" [+++] Objects deleted:", res[1])
+            total_deleted = 0
+            
+            log.info(f" >>> Deleting UP TO {blocks_deleting} blocks between (and including) {start_block} and {end_block}")
+            del_chunk = 2000
+            if blocks_deleting > del_chunk:
+                log.info(f" >>> More than {del_chunk} blocks to be deleted. Running deletion in chunked transactions of {del_chunk}.")
+                curr_start = start_block
+                curr_end = start_block + del_chunk
+                
+                while curr_end <= end_block + 1:
+                    log.info(f" -> Deleting 2000 blocks - {curr_start} to {curr_end} ({end_block - curr_start} blocks left)")
+                    with transaction.atomic():
+                        blocks = EOSBlock.objects.filter(number__gte=curr_start, number__lte=curr_end)
+                        res = blocks.delete()
+                        total_deleted += res[0]
+                    log.info(f" [+++] {res[0]} Objects deleted: {res[1]}")
+                    
+                    db.reset_queries()
+                    
+                    if curr_end >= end_block:
+                        log.info(f" !!> Current end block ({curr_end}) is >= final block ({end_block}). Exiting.")
+                        break
+                    
+                    curr_start = curr_end
+                    if (curr_end + del_chunk) > end_block:
+                        curr_end = end_block
+                    else:
+                        curr_end += del_chunk
+            else:
+                log.info(f" >>> Less than {del_chunk} blocks to be deleted. Deleting blocks in single transaction.")
+    
+                with transaction.atomic():
+                    blocks = EOSBlock.objects.filter(number__gte=start_block, number__lte=end_block)
+                    res = blocks.delete()
+                    total_deleted = res[0]
+                    log.info(" [+++] Objects deleted: %s", res[1])
+            log.info(f" [+++] Successfully deleted {total_deleted} blocks between (and including) {start_block} and {end_block}")
 
             return
         print(" !!! ERROR: No valid arguments. You must specify either individual blocks OR --start / --end")
